455596d7bfcdd4ac0312437569d58c9ff1eecc4e
Micael Jerry <hei.jerry.2@gmail.com>
2024-03-15 21:11:39 +0300

    fix: update

--- src/ft_print_pointer.c
+++ src/ft_print_pointer.c
@@ -6,28 +6,24 @@
 /*   By: mfidimal <mfidimal@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 17:10:21 by mfidimal          #+#    #+#             */
-/*   Updated: 2024/03/15 20:33:51 by mfidimal         ###   ########.fr       */
+/*   Updated: 2024/03/15 21:11:07 by mfidimal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/ft_printf.h"
 
-void	ft_write_base_nb(unsigned long long int nbr, char *base, int size, int *len)
+static void	ft_write_base_16_nb(unsigned long long int nbr, char *base, int size, int *len)
 {
 	char			a;
-	unsigned long long int	n;
 
-	n = nbr;
-	if (n >= (unsigned long long int)size)
-	{
-		ft_write_base_nb(n / size, base, size, len);
-	}
-	a = base[n % size];
+	if (nbr >= (unsigned long long int)size)
+		ft_write_base_nb(nbr / size, base, size, len);
+	a = base[nbr % size];
 	ft_putchar_fd(a, 1);
 	*len = *len + 1;
 }
 
-int	ft_putnbr_base(unsigned long long int nbr)
+int	ft_putnbr_base_16(unsigned long long int nbr)
 {
 	char	*base;
 	int		len;
