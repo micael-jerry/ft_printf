d7a242b971ee374f74ce9f72818e074a20efa0fc
Micael Jerry Fidimalala <mfidimal@student.42antananarivo.mg>
2024-03-16 16:53:12 +0300

    fix: update

--- src/ft_print_str.c
+++ src/ft_print_str.c
@@ -6,7 +6,7 @@
 /*   By: mfidimal <mfidimal@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 16:12:17 by mfidimal          #+#    #+#             */
-/*   Updated: 2024/03/15 18:53:20 by mfidimal         ###   ########.fr       */
+/*   Updated: 2024/03/16 16:47:23 by mfidimal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,11 @@
 
 int	ft_print_str(char *str)
 {
+	if (!str)
+	{
+		ft_putstr_fd("(nil)", 1);
+		return (5);
+	}
 	ft_putstr_fd(str, 1);
 	return (ft_strlen((const char *)str));
 }
